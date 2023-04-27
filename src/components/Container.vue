a0436dc91051d7eee8ee3e219add2e6b9c07cfca
personal <shaneel_kumar@live.com>
2023-04-28 09:57:37 +1200

    add container component for consistent layout of sections

---
+++ src/components/Container.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <section>
+    <slot />
+  </section>
+</template>
+
+<style scoped lang="scss">
+section {
+  padding: 1rem 10rem;
+}
+</style>
