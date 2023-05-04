c47c04b0581f0c3e7d631f29addae8b8c1168b82
personal <shaneel_kumar@live.com>
2023-05-05 11:10:11 +1200

    add card icons, content and list rendering of cards within Statistics component

--- src/components/Statistics.vue
+++ src/components/Statistics.vue
@@ -1,11 +1,36 @@
 <script setup lang="ts">
-import Container from './Container.vue' 
+import { ref } from 'vue';
+import Container from './Container.vue'
+import StatCard from './StatCard.vue'
+
+const cardContent = ref([
+  {
+    title: "Brand Recognition",
+    description: "Boost your brand recognition with each click. Generic links don't mean a thing. Brand links help instil confidence in your content.",
+    icon: "/icon-brand-recognition.svg",
+  },
+  {
+    title: "Detailed Records",
+    description: "Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.",
+    icon: "/icon-detailed-records.svg",
+  },
+  {
+    title: "Fully Customizable",
+    description: "Improve brand awareness and content discoverability through customizable links, superchargin audience engagement.",
+    icon: "/icon-fully-customizable.svg",
+  }
+])
 </script>
 
 <template>
   <Container class="container">
     <h3 class="title">Advanced Statistics</h3>
     <p class="description">Track how your links are performing across the web with our advanced statistics dashboard.</p>
+
+    <div class="card-container">
+      <StatCard class="stat-card" v-for="{ title, description, icon } in cardContent" :title="title"
+        :description="description" :icon="icon" />
+    </div>
   </Container>
 </template>
 
@@ -32,5 +57,28 @@ import Container from './Container.vue'
   line-height: 2rem;
   margin-bottom: 2.5rem;
   max-width: 400px;
+}
+
+.card-container {
+  --card-gap: 7rem;
+  display: flex;
+  flex-direction: column;
+  gap: var(--card-gap);
+  margin: 3rem 0;
+}
+
+.stat-card+.stat-card {
+  position: relative;
+
+  &::before {
+    content: '';
+    position: absolute;
+    bottom: 100%;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 10px;
+    height: var(--card-gap);
+    background: var(--cyan);
+  }
 }
 </style>
