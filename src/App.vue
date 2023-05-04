8ac9e71b1f7e054eb0abc4005bdff2bb61203381
personal <shaneel_kumar@live.com>
2023-05-04 15:59:54 +1200

    add url form and mobile styles

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import Navbar from "./components/Navbar.vue"
 import Hero from "./components/Hero.vue"
+import URLForm from "./components/URLForm.vue"
 </script>
 
 <template>
   <div>
     <Navbar />
     <Hero />
+    <URLForm />
   </div>
 </template>
 
---
+++ src/components/URLForm.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import Container from './Container.vue'
+</script>
+
+<template>
+  <Container>
+    <form class="url-form">
+      <input type="text" placeholder="Shorten a link here...">
+      <button>Shorten it!</button>
+    </form>
+  </Container>
+</template>
+
+<style scoped lang="scss">
+.url-form {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  padding: 1.25rem;
+  gap: 1rem;
+  background: var(--dark-violet);
+  border-radius: 1rem;
+
+  &::before {
+    position: absolute;
+    content: '';
+    width: 100%;
+    height: 100%;
+    border-radius: inherit;
+    top: 0;
+    left: 0;
+    background-image: url('/bg-shorten-mobile.svg');
+    background-size: 75%;
+    background-repeat: no-repeat;
+    background-position: top right;
+  }
+
+  input,
+  button {
+    z-index: 10;
+    border-radius: 0.5rem;
+    border: transparent;
+    padding: 1rem;
+  }
+
+  input {
+    color: red;
+  }
+
+  button {
+    background: var(--cyan);
+    color: white;
+    font-weight: 700;
+  }
+}
+</style>
